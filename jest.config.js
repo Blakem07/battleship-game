904bb0b601916eb7c13b959b1d15b318593b44a5
Blakem07 <Blakem2020@gmail.com>
2025-07-24 11:41:05 +0100

    Configured Jest to work with Babel and ESM syntax
    Replaced ESM-style export in jest.config.js with CommonJS Syntax
    
    Updated babel.config.js to use CommonJS Syntax
    
    Ensured babel-jest is used to transpile ES modules
    
    Removed type: module from package.json to avoid ESM conflicts

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,6 @@
-export default {
+module.exports = {
+  testEnvironment: "node",
   transform: {
-    "^.+\\.jsx?$": "babel-jest", // Transforms .js and .jsx files using babel-jest
+    "^.+\\.js$": "babel-jest",
   },
-  watchPathIgnorePatterns: [
-    "<rootDir>/node_modules/",
-    "<rootDir>/dist/",
-    "<rootDir>/coverage/",
-    "<rootDir>/.vscode/",
-    "\\.git/",
-    "\\.DS_Store",
-  ],
 };
