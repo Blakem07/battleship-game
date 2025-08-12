2d9746398efe6aba2e75a44dacd4c68ce316f405
Blake <Blakem2020@gmail.com>
2025-08-12 17:10:25 +0100

    WIP: add test fot computer.randomAttack

--- src/tests/computer.test.js
+++ src/tests/computer.test.js
@@ -6,3 +6,12 @@ test("Computer is intialized with an instance of player.", () => {
 
   expect(computer.player).toBe(mockPlayer);
 });
+
+// Tests for attack method
+
+test("Computer.randomAttack method calls player.attack with random values.", () => {
+  const mockPlayer = {attack: jest.fn()};
+  const computer = new Computer();
+
+  randomNumber =
+});
