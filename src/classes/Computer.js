da98085d779c5b40feea9a5f8a03b9ffe747214b
Blake <Blakem2020@gmail.com>
2025-08-12 17:22:50 +0100

    feat: added computer.getRandomInt

--- src/classes/Computer.js
+++ src/classes/Computer.js
@@ -2,4 +2,17 @@ export default class Computer {
   constructor(player) {
     this.player = player;
   }
+
+  /**
+   * getRandomInt method.
+   *
+   * Returns a number between a min and a max.
+   *
+   * @param {number} - Mininum limit
+   * @param {number} - Maximum limit
+   * @return {number} - Random number.
+   */
+  getRandomInt(min, max) {
+    return Math.random() * (max - min) + min;
+  }
 }
