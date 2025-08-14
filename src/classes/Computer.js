861513770921751457961f096094c59b8789a63c
Blakem07 <Blakem2020@gmail.com>
2025-08-14 15:58:06 +0100

    feat: add computer.randomAttack method.

--- src/classes/Computer.js
+++ src/classes/Computer.js
@@ -4,6 +4,21 @@ export default class Computer {
   }
 
   /**
+   * randomAttack method.
+   *
+   * Calls player.attack with randomly generated numbers.
+   *
+   * @param {object} - Target of the attack.opponent
+   * @returns {*} - Result of player.attack()
+   */
+  randomAttack(opponent) {
+    const rX = this.getRandomInt(0, 10);
+    const rY = this.getRandomInt(0, 10);
+
+    return this.player.attack(opponent, rX, rY);
+  }
+
+  /**
    * getRandomInt method.
    *
    * Returns a number between a min and a max.
