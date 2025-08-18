464f1761259b5db2f0a536a411fb72262504eb4c
Blakem07 <Blakem2020@gmail.com>
2025-08-18 15:52:36 +0100

    feat: add currentTurn and gameOver to GameController object

--- src/classes/GameController.js
+++ src/classes/GameController.js
@@ -2,5 +2,7 @@ export default class GameController {
   constructor(player, computer) {
     this.player = player;
     this.computer = computer;
+    this.currentTurn = "player";
+    this.gameOver = false;
   }
 }
