7f3c28817150582ab58a40c003f128b5d01ea92e
Blakem07 <Blakem2020@gmail.com>
2025-08-18 15:47:31 +0100

    refactor(GameController): rename player1 to player in GameController and tests.

--- src/classes/GameController.js
+++ src/classes/GameController.js
@@ -1,6 +1,6 @@
 export default class GameController {
-  constructor(player1, computer) {
-    this.player1 = player1;
+  constructor(player, computer) {
+    this.player = player;
     this.computer = computer;
   }
 }
--- src/tests/gamecontroller.test.js
+++ src/tests/gamecontroller.test.js
@@ -6,6 +6,6 @@ test("GameController is initialized with references to Player1 and Computer.", (
 
   const gameController = new GameController(mockPlayer, mockComputer);
 
-  expect(gameController.player1).toBe(mockPlayer);
+  expect(gameController.player).toBe(mockPlayer);
   expect(gameController.computer).toBe(mockComputer);
 });
