4317c59fe841b113ce36f628c1136b012ea37f12
Blakem07 <Blakem2020@gmail.com>
2025-08-19 15:23:54 +0100

    feat: computer.placeShipsRandomly working for basic test

--- src/classes/Computer.js
+++ src/classes/Computer.js
@@ -19,6 +19,38 @@ export default class Computer {
   }
 
   /**
+   * placeShipsRandomly method.
+   *
+   * Handles the ship placement of the entire fleet using randomly generated numbers.
+   *
+   */
+  placeShipsRandomly() {
+    const shipNames = [
+      "carrier",
+      "battleship",
+      "cruiser",
+      "submarine",
+      "destroyer",
+    ];
+
+    const shipLengths = {
+      carrier: 5,
+      battleship: 4,
+      cruiser: 3,
+      submarine: 3,
+      destroyer: 2,
+    };
+
+    shipNames.forEach((shipName) => {
+      let rX = this.getRandomInt(0, 10);
+      let rY = this.getRandomInt(0, 10);
+      let direction = rX % 2 === 0 ? "horizontal" : "vertical";
+
+      this.player.placeShip(rX, rY, shipLengths[shipName], shipName, direction);
+    });
+  }
+
+  /**
    * getRandomInt method.
    *
    * Returns a number between a min and a max.
