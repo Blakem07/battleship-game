370a239c34bd7a50920a14c5fc70458ba4315aab
Blake <Blakem2020@gmail.com>
2025-08-20 11:17:01 +0100

    feat: add place GameController.placeAllShips passing basic test.

--- src/classes/GameController.js
+++ src/classes/GameController.js
@@ -5,4 +5,18 @@ export default class GameController {
     this.currentTurn = "player";
     this.gameOver = false;
   }
+  
+  /**
+   * placeAllShips method.
+   * 
+   * This method places all ships for the player based on ship
+   * positions passed to it by the UI.
+   * 
+   * @param {Array} - shipPositions passed from UI
+  */
+  placeAllShips(shipPositions) {
+    shipPositions.forEach((shipPosition) => {
+      this.player.placeShip(shipPosition);
+    });
+  }
 }
