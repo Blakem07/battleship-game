0e617d03fd74a7a24526265fa9c32ac8dcc48ec5
Blake <Blakem2020@gmail.com>
2025-08-21 12:57:24 +0100

    refactor: Ship Class so that it is intialized using ship name and sets length internally

--- src/classes/Ship.js
+++ src/classes/Ship.js
@@ -1,18 +1,42 @@
 export default class Ship {
-  constructor(length = 5) {
-    this.length = length;
+  static VALID_NAMES = [
+    "carrier",
+    "battleship",
+    "cruiser",
+    "submarine",
+    "destroyer",
+  ];
+
+  static VALID_LENGTHS = {
+    carrier: 5,
+    battleship: 4,
+    cruiser: 3,
+    submarine: 3,
+    destroyer: 2,
+  };
+
+  constructor(name) {
+    this.name = name;
     this.timesHit = 0;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    const lowerCaseValue = value.toLowerCase();
+
+    if (!Ship.VALID_NAMES.includes(lowerCaseValue)) {
+      throw new Error("Error the ship name is invalid.");
+    }
+
+    this._name = lowerCaseValue;
+    this._length = Ship.VALID_LENGTHS[lowerCaseValue];
   }
 
   get length() {
     return this._length;
-  }
-
-  set length(value) {
-    if (value < 2 || value > 5) {
-      throw new Error("Invalid length: must be between 2 and 5.");
-    }
-    this._length = value;
   }
 
   hit() {
