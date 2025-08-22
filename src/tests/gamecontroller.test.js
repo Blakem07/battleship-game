58dcb22bf24c67a412bd53b3df9d6e505ec040e2
Blakem07 <Blakem2020@gmail.com>
2025-08-22 15:41:51 +0100

    test: updated gameController tests to work with Ship Class refactor
    NOTE: GameController.placeAllShips still WIP

--- src/tests/gamecontroller.test.js
+++ src/tests/gamecontroller.test.js
@@ -1,6 +1,7 @@
+import Ship from "../classes/Ship";
 import GameController from "../classes/GameController";
 
-describe.skip("GameController Class Tests", () => {
+describe("GameController Class Tests", () => {
   test("GameController is initialized with references to Player and Computer.", () => {
     const mockPlayer = {};
     const mockComputer = {};
@@ -23,36 +24,31 @@ describe.skip("GameController Class Tests", () => {
       {
         row: 0,
         col: 0,
-        length: 5,
-        shipName: "carrier",
+        shipName: Ship.VALID_NAMES[0],
         direction: "horizontal",
       }, // occupies (0,0) to (0,4)
       {
         row: 2,
         col: 0,
-        length: 4,
-        shipName: "battleship",
+        shipName: Ship.VALID_NAMES[1],
         direction: "vertical",
       }, // occupies (2,0) to (5,0)
       {
         row: 5,
         col: 2,
-        length: 3,
-        shipName: "destroyer",
+        shipName: Ship.VALID_NAMES[2],
         direction: "horizontal",
       }, // occupies (5,2) to (5,4)
       {
         row: 7,
         col: 5,
-        length: 3,
-        shipName: "submarine",
+        shipName: Ship.VALID_NAMES[3],
         direction: "vertical",
       }, // occupies (7,5) to (9,5)
       {
         row: 9,
         col: 7,
-        length: 2,
-        shipName: "patrol",
+        shipName: Ship.VALID_NAMES[4],
         direction: "horizontal",
       }, // occupies (9,7) to (9,8)
     ];
@@ -76,7 +72,6 @@ describe.skip("GameController Class Tests", () => {
       {
         row: 0,
         col: 0,
-        length: 5,
         shipName: "carrier",
         direction: "horizontal",
       },
@@ -93,7 +88,7 @@ describe.skip("GameController Class Tests", () => {
 
   // Tests for GameController.placeAllShips
 
-  test("GameController.placeAllShips places all ships correctly on the board.", () => {
+  test.skip("GameController.placeAllShips places all ships correctly on the board.", () => {
     /***
      * TODO:
      * - Verify placePlayerShips is called with the correct input[x].
