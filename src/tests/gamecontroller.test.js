b83bff18ba78ba6497d2772032ac5090dff65ab5
Blakem07 <Blakem2020@gmail.com>
2025-08-22 16:13:55 +0100

    WIP: GameController tests

--- src/tests/gamecontroller.test.js
+++ src/tests/gamecontroller.test.js
@@ -88,10 +88,11 @@ describe("GameController Class Tests", () => {
 
   // Tests for GameController.placeAllShips
 
-  test.skip("GameController.placeAllShips places all ships correctly on the board.", () => {
+  test("GameController.placeAllShips places all Player and Computer Ships correctly on the board.", () => {
     /***
-     * TODO:
+     * TODO: WORK IN PROGRESS....
      * - Verify placePlayerShips is called with the correct input[x].
+     * - Verify placeComputerShips is called with the correct input[x]
      * - Write a method in gameboard to return all placed ships.
      *        a. So that we can check that the number of ships place is correct.
      * - Check all positions are within bounds.
@@ -102,44 +103,44 @@ describe("GameController Class Tests", () => {
       {
         row: 0,
         col: 0,
-        length: 5,
         shipName: "carrier",
         direction: "horizontal",
       }, // occupies (0,0) to (0,4)
       {
         row: 2,
         col: 0,
-        length: 4,
         shipName: "battleship",
         direction: "vertical",
       }, // occupies (2,0) to (5,0)
       {
         row: 5,
         col: 2,
-        length: 3,
         shipName: "destroyer",
         direction: "horizontal",
       }, // occupies (5,2) to (5,4)
       {
         row: 7,
         col: 5,
-        length: 3,
         shipName: "submarine",
         direction: "vertical",
       }, // occupies (7,5) to (9,5)
       {
         row: 9,
         col: 7,
-        length: 2,
         shipName: "patrol",
         direction: "horizontal",
       }, // occupies (9,7) to (9,8)
     ];
-
     const placePlayerShipsSpy = jest.spyOn(gameController, "placePlayerShips");
+    const placeShipsRandomlySpy = jest.spyOn(computer, "placeShipsRandomly"); // Computer
 
     gameController.placeAllShips(shipPositions);
 
-    expect(placePlayerShipsSpy).toHaveBeenNthCalledWith(shipPositions);
+    shipPositions.forEach((position, index) => {
+      expect(placePlayerShipsSpy).toHaveBeenNthCalledWith(index + 1, position);
+    });
+    expect(placeShipsRandomlySpy).toHaveBeenCalledTimes(1);
+
+
   });
 });
