bb69d7305032c1e3ed9a9937fc56794b58d76878
Blakem07 <Blakem2020@gmail.com>
2025-09-07 11:51:23 +0100

    test: add test for UI.populateGrid ensuring the grid recieves the correct number of row elements.

--- src/tests/ui.test.js
+++ src/tests/ui.test.js
@@ -8,21 +8,28 @@ import UI from "../classes/UI.js";
 describe("UI Class Tests", () => {
   let ui;
 
+  let createCellMock;
+
+  let rows;
+  let cols;
+
+  let gridContainer;
+
   beforeEach(() => {
     ui = new UI();
+
+    createCellMock = jest.fn();
+
+    rows = Gameboard.BOARD_ROWS; // 10
+    cols = Gameboard.BOARD_COLS; // 10
+
+    gridContainer = document.createElement("div");
   });
   afterEach(() => {
     jest.restoreAllMocks();
   });
 
   test("UI.populateGrid calls createCell for the correct amount rows and cols.", () => {
-    const createCellMock = jest.fn();
-
-    const rows = Gameboard.BOARD_ROWS; // 10
-    const cols = Gameboard.BOARD_COLS; // 10
-
-    const gridContainer = document.createElement("div");
-
     ui.populateGrid(gridContainer, {
       row: rows,
       col: cols,
@@ -32,5 +39,15 @@ describe("UI Class Tests", () => {
     expect(createCellMock).toHaveBeenCalledTimes(rows * cols);
     expect(createCellMock).toHaveBeenCalledWith(0, 0); // First
     expect(createCellMock).toHaveBeenCalledWith(rows - 1, cols - 1); // Last
+  });
+
+  test("UI.populateGrid has the grid recieve the correct number of row elements.", () => {
+    ui.populateGrid(gridContainer, {
+      row: rows,
+      col: cols,
+      createCell: createCellMock,
+    });
+
+    expect(gridContainer.children.length).toEqual(rows);
   });
 });
